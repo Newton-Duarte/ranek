a11bf4430e9d5b38182b5f61fb12a6367b4a68f4
Newton Darte <newton_@hotmail.com>
2020-04-05 22:27:33 -0300

    View Home created with products list and products search components.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,101 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Ranek</router-link>
-    </div>
-    <router-view/>
+    <TheHeader />
+    <main id="main">
+      <router-view/>
+    </main>
+    <TheFooter />
   </div>
 </template>
 
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+import TheFooter from '@/components/TheFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body, ul, li, h1, h2, p {
+  padding: 0;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #345;
+  background: url("~@/assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, .2);
+  transition: all .3s;
+  border: none;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
+
+input, textarea {
+  border-radius: 4px;
+  border: 1px solid white;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, .1);
+  transition: all .3s;
+  font-size: 1rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  margin-bottom: 15px;
+}
+
+input:hover, input:focus,
+textarea:hover, textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, .2);
+  border-collapse: #87f;
+}
 </style>
